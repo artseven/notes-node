be9d9e2a39661489e2ff65ba25536ad2cf379f1a
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-02-05 16:51:42 -0500

    cleanup

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 console.log('Starting app.js');
 
 const fs    = require('fs');
-const os    = require('os');
+const _     = require('lodash');
+
 const notes = require('./notes');
 
-var res = notes.addNote();
-console.log(res);
-
-var results = notes.add(3, 7);
-console.log('Result:', results);
-// var user = os.userInfo();
-// // console.log(user);
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, (err) =>{
-//     if (err) {
-//         console.log('Unable to write to file');
-//     }
-// });
