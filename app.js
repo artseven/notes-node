9b3487f91ed781190b3917c91e42382058353178
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-02-05 13:06:40 -0500

    ES6 support added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const os = require('os');
 
 var user = os.userInfo();
 // console.log(user);
-fs.appendFile('greetings.txt', 'Hello ' + user.username + '!', (err) =>{
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, (err) =>{
     if (err) {
         console.log('Unable to write to file');
     }
