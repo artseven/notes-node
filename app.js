b1f20ef0ac0a4ad42ffe814769040654d12fa0b9
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-02-05 13:10:44 -0500

    Adding notes.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-console.log('Starting app');
+console.log('Starting app.js');
 
-const fs = require('fs');
-const os = require('os');
+const fs    = require('fs');
+const os    = require('os');
+const notes = require('./notes');
 
 var user = os.userInfo();
 // console.log(user);
