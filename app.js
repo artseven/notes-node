e06f9e2f922e72a4e66061d572a3a9ac09b43080
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-02-05 13:28:37 -0500

    Function as a part of module exports

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const fs    = require('fs');
 const os    = require('os');
 const notes = require('./notes');
 
-var user = os.userInfo();
-// console.log(user);
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, (err) =>{
-    if (err) {
-        console.log('Unable to write to file');
-    }
-});
+var res = notes.addNote();
+console.log(res);
+// var user = os.userInfo();
+// // console.log(user);
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, (err) =>{
+//     if (err) {
+//         console.log('Unable to write to file');
+//     }
+// });
